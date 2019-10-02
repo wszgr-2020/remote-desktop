c2d2decf49792195040b37bcf13c3789ef97bf6e
animeshxd <47624427+animeshxd@users.noreply.github.com>
2019-10-02 04:34:23 +0000

    Add files via upload
    Remote desktop connection
    using chrome remote desktop and xrdp

---
+++ xfce4_crdp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt update
+echo "system is now updated"
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install --assume-yes xfce4 desktop-base
+echo "xfce4-session" > ~/.chrome-remote-desktop-session
+sudo apt install --assume-yes xscreensaver
+sudo apt install --assume-yes xfce4-terminal
+sudo systemctl disable lightdm.service
+echo "export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES=1280x720,1368x786" \
+    >> ~/.profile
+sudo systemctl restart chrome-remote-desktop
+sudo passwd $(whoami)    
+echo "visit https://remotedesktop.google.com/headless"

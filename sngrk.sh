db4ad57813ebd4d14e92ccf0f4bc47997d1ae2d9
animeshxd <47624427+animeshxd@users.noreply.github.com>
2020-04-23 10:30:35 +0530

    Update sngrk.sh

--- sngrk.sh
+++ sngrk.sh
@@ -5,7 +5,7 @@ output=$(curl -s http://localhost:4040/api/tunnels)
 if [ -z "$output" ]
 then
   echo "Tunnel down. Starting new tunnel..."
-  nohup ngrok tcp 3389 &
+  nohup ./ngrok tcp 3389 &
   echo "Tunnel started."
 else
   echo "Tunnel running."

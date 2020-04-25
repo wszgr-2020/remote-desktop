d8cc45e67d581a351d46c63de99d62a7291f42a7
animeshxd <47624427+animeshxd@users.noreply.github.com>
2020-04-25 10:03:39 +0530

    Update crdp.sh

--- crdp.sh
+++ crdp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb --no-check-certificate
 sudo apt update
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
@@ -9,7 +9,7 @@ echo "xfce4-session" > ~/.chrome-remote-desktop-session
 sudo apt install --assume-yes xscreensaver
 sudo apt install --assume-yes task-xfce-desktop
 sudo systemctl disable lightdm.service
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb --no-check-certificate
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 echo "export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES=1280x720,1368x786" \
